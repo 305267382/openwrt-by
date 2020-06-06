40c28dfd514793154ee6e3f89c3dbe99c7ab2358
305267382 <53901698+305267382@users.noreply.github.com>
2020-06-06 17:47:42 +0800

    20200606

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-#=================================================
-# Description: DIY script
+#=============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
 # Lisence: MIT
 # Author: P3TERX
 # Blog: https://p3terx.com
-#=================================================
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#=============================================================
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
