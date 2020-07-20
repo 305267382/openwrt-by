0708fd68539c52c207b829fd2b1e5ead92c9722b
305267382 <53901698+305267382@users.noreply.github.com>
2020-07-20 12:14:49 +0800

    20200720

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,12 @@
 # Add a feed source
 
 
-sed -i '$a src-git xiaoqingfeng https://github.com/xiaoqingfengATGH/feeds-xiaoqingfeng' feeds.conf.default
+
 sed -i '$a src-git node https://github.com/nxhack/openwrt-node-packages.git' feeds.conf.default
 sed -i '$a src-git telephony https://git.openwrt.org/feed/telephony.git;openwrt-19.07' feeds.conf.default
-
+sed -i '$a src-git video https://github.com/openwrt/video.git' feeds.conf.default
+sed -i '$a src-git targets https://github.com/openwrt/targets.git' feeds.conf.default
+sed -i '$a src-git management https://github.com/openwrt-management/packages.git' feeds.conf.default
+sed -i '$a src-git oldpackages http://git.openwrt.org/packages.git' feeds.conf.default
+sed -i '$a src-link custom /usr/src/openwrt/custom-feed' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
