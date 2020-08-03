0fec6d93983ce6a825098f830c7dedc55ebe93fa
305267382 <53901698+305267382@users.noreply.github.com>
2020-08-03 08:25:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,3 +22,7 @@ sed -i '$a src-git management https://github.com/openwrt-management/packages.git
 #sed -i '$a src-git oldpackages http://git.openwrt.org/packages.git' feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 sed -i '$a src-git xiaoqingfeng https://github.com/xiaoqingfengATGH/feeds-xiaoqingfeng.git' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/xiaoqingfengATGH/feeds-lienol.git' feeds.conf.default
+sed -i '$a src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' feeds.conf.default
+sed -i '$a src-git homeclash https://github.com/xiaoqingfengATGH/homeclash.git' feeds.conf.default
+sed -i '$a src-git strongswanInDocker https://github.com/xiaoqingfengATGH/luci-app-strongswanInDocker.git' feeds.conf.default
