f83243d80313220888b321265e15388c7388cdcf
305267382 <53901698+305267382@users.noreply.github.com>
2020-09-22 15:31:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,10 +16,10 @@
 
 
 sed -i '$a src-git node https://github.com/nxhack/openwrt-node-packages.git' feeds.conf.default
-#sed -i '$a src-git video https://github.com/openwrt/video.git' feeds.conf.default
+sed -i '$a src-git video https://github.com/openwrt/video.git' feeds.conf.default
 sed -i '$a src-git targets https://github.com/openwrt/targets.git' feeds.conf.default
 sed -i '$a src-git management https://github.com/openwrt-management/packages.git' feeds.conf.default
-#sed -i '$a src-git oldpackages http://git.openwrt.org/packages.git' feeds.conf.default
+sed -i '$a src-git oldpackages http://git.openwrt.org/packages.git' feeds.conf.default
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 #sed -i '$a src-git xiaoqingfeng https://github.com/xiaoqingfengATGH/feeds-xiaoqingfeng.git' feeds.conf.default
 #sed -i '$a src-git lienol https://github.com/xiaoqingfengATGH/feeds-lienol.git' feeds.conf.default
